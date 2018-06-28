a36ef2b078662e7785e0ef88dba3619286593a91
Sheila Wittman <szandora789@gmail.com>
2018-06-28 20:49:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,13 +3,13 @@ require_relative "../config/environment.rb"
 class Student
 
 attr_accessor :name, :grade
-attr_reader :id 
+attr_reader :id
 
 def initialize(name, grade, id)
   @name = name
   @grade = grade
-  @id = nil 
-end 
+  @id = nil
+end
 
 def self.create_table
   sql = <<-SQL
@@ -20,19 +20,19 @@ def self.create_table
   )
   SQL
   DB[:conn].execute(sql)
-end 
+end
 
 def self.drop_table
   sql = <<-SQL
   DROP TABLE IF EXISTS students
   SQL
   DB[:conn].execute(sql)
-end 
+end
 
-def save 
-  if self.id 
-    self.update 
-  else 
+def save
+  if self.id
+    self.update
+  else
     sql = <<-SQL
     INSERT INTO students (name, grade)
     VALUES (?, ?)
@@ -40,9 +40,9 @@ def save
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
-end 
+end
 
-def update 
+def update
   sql = <<-SQL
   UPDATE students SET name = ?, grade = ? WHERE id = ?
   SQL
@@ -53,12 +53,12 @@ def self.create(name, grade)
   student = Student.new(name, grade)
   student.save
   student
-end 
+end
 
 def self.new_from_db(row)
   student_new = self.new(row[0], row[1], row[2])
   student_new
-end 
+end
 
 def self.find_by_name(name)
   sql = <<-SQL
@@ -68,4 +68,4 @@ def self.find_by_name(name)
     self.new_from_db(row)
   end.first
 end
-
+end 
